1acd78726a25692b5bc7929fe3aa79b90694fffd
reginaldo.marinho <reginaldomarinho2016@gmail.com>
2022-09-22 17:05:55 -0300

    commit

--- DocWriteConsole/Program.cs
+++ DocWriteConsole/Program.cs
@@ -8,7 +8,27 @@ namespace DocWriteConsole
     {
         static void Main(string[] args)
         {
-            NovoProjeto.CriarNovoProjeto("/home/reginaldo/Desenvolvimento/","adonix","doc");
+            var rootCommand = new RootCommand();
+            var docCommmand = new Command("doc", "Commando inicial para todos dos outros comandos");
+            rootCommand.Add(docCommmand);
+            var docNewCommmand =  new Command("new", "Indica a criação de um novo projeto"); // doc new
+            docCommmand.Add(docNewCommmand);
+            var docDelCommmand =  new Command("remove", "Remove um projeto criado"); // doc remove
+            docCommmand.Add(docDelCommmand); // doc new --path
+            var path = new Option<string>(
+                name: "--path",
+                description: "recebe o caminho dos projetos desejados"
+                
+            ){IsRequired = true};
+            docNewCommmand.Add(path);
+            var project = new Option<string>(
+                name: "--project",
+                description: "recebe o nome do projeto"
+            ){IsRequired = true};
+            docNewCommmand.Add(project);
+
+            string[] argsTest = {"doc","new","--path","scscscscscsc"};
+            rootCommand.Invoke(argsTest);
         }
 
     }
