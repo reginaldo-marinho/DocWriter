54148826b8a9d48d7744afe9fd143a18f9143153
reginaldo.marinho <reginaldomarinho2016@gmail.com>
2022-10-12 14:49:00 -0300

    commit

--- DocWrite/Diretorios/NovoProjeto.cs
+++ DocWrite/Diretorios/NovoProjeto.cs
@@ -13,21 +13,12 @@ public class NovoProjeto
           CriacaoPasta.ChecarCriarDiretorio($"{path}/{projeto}".Replace("//","/"));
           CreateFileConfigurationPages(path,projeto,tamplete);
           CriarContentsDefult(path,projeto);
-          CopyContentsBase(path,projeto);    
      }
      private static void CriarContentsDefult(string path,string projeto){
           CriacaoPasta.ChecarCriarDiretorio($"{path}/{projeto}/pages".Replace("//","/"));
           CriacaoPasta.ChecarCriarDiretorio($"{path}/{projeto}/lc".Replace("//","/"));
-          CriacaoArquivo.ChecarCriarArquivo($"{path}/{projeto}/lc/lc.html".Replace("//","/"));
+          CriacaoArquivo.ChecarCriarArquivo($"{path}/{projeto}/lc/lc.json".Replace("//","/"));
           CriacaoArquivo.ChecarCriarArquivo($"{path}/{projeto}/lc/lc.fogx".Replace("//","/"));
-     }
-     private static void CopyContentsBase(string path,string projeto){
-          var pathbase = $"{PathBase.GetPathBaseDocWriter()}/DocWrite/Files-base";
-          CopiaArquivosBase.CorpiarArquivosBase($"{pathbase}/documentation.css",$"{path}/{projeto}/documentation.css");
-          CopiaArquivosBase.CorpiarArquivosBase($"{pathbase}/documentation.js",$"{path}/{projeto}/documentation.js");
-          CopiaArquivosBase.CorpiarArquivosBase($"{pathbase}/index.html",$"{path}/{projeto}/index.html");
-          CopiaArquivosBase.CorpiarArquivosBase($"{pathbase}/stylefogx.css",$"{path}/{projeto}/stylefogx.css");
-          CopiaArquivosBase.CorpiarArquivosBase($"{pathbase}/init.json",$"{path}/{projeto}/init.json");          
      }
      private static void CreateFileConfigurationPages(string path,string projeto,string tamplete){
           StringBuilder build = new StringBuilder();
